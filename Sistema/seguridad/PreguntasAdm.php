d8956647e1e53012787c16d13aeca0398d35b712
FernandoGalo <fernando.galo@unah.hn>
2023-08-08 23:25:53 -0600

    permisos preguntas

--- Sistema/seguridad/PreguntasAdm.php
+++ Sistema/seguridad/PreguntasAdm.php
@@ -6,7 +6,7 @@ $model = new EVENT_BITACORA;
 session_start();
 $model->entrarpreg();  
 $usuario=$_SESSION['usuario'];
-$ID_Rol=$_SESSION['ID_Rol'];
+$ID_Rol=$_SESSION['ID_Rol']; 
 
 if (empty($_SESSION['user']) and empty($_SESSION['ID_User'])) {
     header('location:../../Pantallas/Login.php');
@@ -49,7 +49,7 @@ if (empty($_SESSION['user']) and empty($_SESSION['ID_User'])) {
                   <div class="box">
                     <div class="box-header with-border">
                           <h1 class="box-title" style="text-align:center; margin-top:15px; margin-bottom:20px">Mantenimiento de Preguntas</h1>
-                          <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Insercion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
+                          <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where (Permiso_Insercion=1 and ID_Rol=$ID_Rol and ID_Objeto=7) or ('$usuario'='ADMIN')");
 if ($datos=$sql->fetch_object()) { ?>
 
                           <button class="btn btn-success" id="btnagregar" name="btnAgregar" onclick="mostrarform(true)"><i class="zmdi zmdi-money-box"></i> Agregar Pregunta</button>
@@ -66,7 +66,7 @@ if ($datos=$sql->fetch_object()) { ?>
                     </div>
                     <!-- /.box-header -->
                     <!-- centro -->
-                    <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_consultar=1 and ID_Rol=$ID_Rol and ID_Objeto=7 ");
+                    <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where (Permiso_consultar=1 and ID_Rol=$ID_Rol and ID_Objeto=7) or ('$usuario'='ADMIN') ");
 if ($datos=$sql->fetch_object()) { ?>
 <div class="panel-body" id="listadoregistros">
 <main>
@@ -119,11 +119,11 @@ document.getElementById("campo").addEventListener("keyup", function(event) {
                             <th class="sort asc">Preguntas</th>
                             <th class="sort asc">Acciones</th>
 
-                            <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Actualizacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
+                            <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where (Permiso_Actualizacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7) or ('$usuario'='ADMIN')");
 if ($datos=$sql->fetch_object()) { ?>
                             <th></th>
                             <?php } ?>
-                            <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where Permiso_Eliminacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7");
+                            <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where (Permiso_Eliminacion=1 and ID_Rol=$ID_Rol and ID_Objeto=7) or ('$usuario'='ADMIN')");
 if ($datos=$sql->fetch_object()) { ?>
                             <th></th>
                             <?php } ?>
