b45332e415a39a11035e15abd9aa3e4ce145aef7
Jrivera99 <110574944+Jrivera99@users.noreply.github.com>
2023-08-09 17:25:35 -0600

    Update usuariosAdm.php
    Estandarizacion de titulos

--- Sistema/Usuarios/usuariosAdm.php
+++ Sistema/Usuarios/usuariosAdm.php
@@ -60,7 +60,7 @@ $R_F_Vencida= date("Y-m-j",strtotime($R_Fecha_actual."+ ".$diasV." days")); /*le
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                          <h1 style="text-align:center; margin-top:15px; margin-bottom:20px" class="box-title">Mantenimiento usuarios</h1>
+                          <h1 style="text-align:center; margin-top:15px; margin-bottom:20px" class="box-title">Mantenimiento Usuarios</h1>
                           <?php $sql=$conexion->query("SELECT * FROM tbl_permisos where (Permiso_Insercion=1 and ID_Rol=$ID_Rol and ID_Objeto=1) or ('$usuario'='ADMIN')");
 if ($datos=$sql->fetch_object()) { ?>
                           <button class="btn btn-success" id="btnagregar" name="btnAgregar" onclick="mostrarform(true)"><i class="zmdi zmdi-account-add"></i>Agregar Usuario</button>
