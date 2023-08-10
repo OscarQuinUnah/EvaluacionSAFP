70ab32c1afd5750e95b75f254353cfb29fe6b335
Jrivera99 <110574944+Jrivera99@users.noreply.github.com>
2023-08-09 18:40:12 -0600

    Update PreguntasAdm.php

--- Sistema/seguridad/PreguntasAdm.php
+++ Sistema/seguridad/PreguntasAdm.php
@@ -253,7 +253,7 @@ if ($datos=$sql->fetch_object()) { ?>
                           <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
                             <label>Pregunta(*):</label>
                             <input type="hidden" name="Pregunta" id="Pregunta">
-                            <input oncopy="return false" onpaste="return false" type="text" class="form-control" name="Pregunta" id="Pregunta" maxlength="50" placeholder="Ingrese una pregunta" oninput="this.value = this.value.toUpperCase();" onkeypress="return validarEspaciosYSignos(event)" required>
+                            <input oncopy="return false" onpaste="return false" type="text" class="form-control" name="Pregunta" id="Pregunta" maxlength="50" placeholder="INGRESE UNA PREGUNTA" oninput="this.value = this.value.toUpperCase();" onkeypress="return validarEspaciosYSignos(event)" required>
                           </div>
                         
                           <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
